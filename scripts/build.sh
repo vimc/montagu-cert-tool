eb23f96d37bcce1c953dc8e42c7029886990ae25
Alex <alex.hill@gmail.com>
2017-11-15 12:00:33 +0000

    Update build.sh
    mount docker auth

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,10 @@ set -e
 git_id=$(git rev-parse --short HEAD)
 git_branch=$(git symbolic-ref --short HEAD)
 
+# This is the path for teamcity agents. If running locally, pass in your own docker config location
+# i.e. /home/{user}/.docker/config.json
+docker_auth_path=${1:-/opt/teamcity-agent/.docker/config.json}
+
 docker build \
     --tag montagu-cert-tool-build \
     --build-arg git_id=$git_id \
@@ -11,5 +15,6 @@ docker build \
     .
 
 docker run --rm \
+    -v $docker_auth_path:/root/.docker/config.json \
     -v /var/run/docker.sock:/var/run/docker.sock \
     montagu-cert-tool-build
